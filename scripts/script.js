e34451b7ba40e56f2ddf5ea0e0bdf41e5dfd1a64
Diana Gerasimenko <DiaGerasimenko@yandex.ru>
2020-10-27 22:08:10 +0300

    Место Россия

--- scripts/script.js
+++ scripts/script.js
@@ -7,14 +7,14 @@ const title = document.querySelector('.profile__title');
 const subTitle = document.querySelector('.profile__subtitle');
 const form = document.querySelector('.popup__form');
 const nameField = document.querySelector('.popup__input_type_name');
-
+const titleField = document.querySelector('.popup__input_type_title');
 
 function showPopup() {
   popup.classList.add('popup_opened');
-  popup.removeEventListener('click', showPopup)
+  popup.removeEventListener('click', showPopup);
+  title.value = nameField.textContent;
+  subTitle.value = titleField.textContent;
 }
-
-
 function closePopup() {
   popup.classList.remove('popup_opened')
 }
@@ -24,15 +24,10 @@ popupCloseButton.addEventListener('click', closePopup);
 
 function submitForm(event) {
   event.preventDefault();
-
- 
   title.textContent = nameField.value;
-
-
+  subTitle.textContent = titleField.value;
   closePopup();
 }
 
-
 form.addEventListener('submit', submitForm);
-
 
